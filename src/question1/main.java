be83428e558addfb5a4df47914b37de03c6c9ec5
tarundeepSingh11 <tarundeep.singh@tothenew.com>
2023-03-02 15:22:35 +0530

    changes

--- src/question1/main.java
+++ src/question1/main.java
@@ -5,19 +5,19 @@ public class main {
     {
         SBI sbi = new SBI();
         sbi.showBank();
-        sbi.SBISetter("State Bank of India", "N-17 ABC", "Akash", 22, 5, 9.2, 14);
+        sbi.SBISetter("State Bank of India", "motinagar", "karan", 22, 5, 8.2, 7);
         sbi.SBIGetter();
         System.out.println(sbi.toString());
 
         BOI boi = new BOI();
         boi.showBank();
-        boi.BOISetter("Bank of India", "A-55 MNO", "Dilip", 14, 6.5, 10.8, 15);
+        boi.BOISetter("Bank of India", "kirti nagar", "tarun", 15, 9, 14, 10);
         boi.BOIGetter();
         System.out.println(boi.toString());
 
         ICICI icici = new ICICI();
         icici.showBank();
-        icici.ICICISetter("ICICI", "C-19 XYZ", "Girver", 18, 7, 8.9, 12.5);
+        icici.ICICISetter("ICICI", "patel nagar", "Anuj", 14, 4.45, 8, 16);
         icici.ICICIGetter();
         System.out.println(icici.toString());
     }
